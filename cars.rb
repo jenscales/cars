0c81ff735d8376a95da53b27c13751bde9d3093e
Jennifer Scales <jen_scales@yahoo.com>
2013-11-18 18:40:09 -0600

    working on step 8

--- cars.rb
+++ cars.rb
@@ -1,7 +1,11 @@
 class Car
 	@@total_car_count = 0
-
+	@@cars_per_color[color: count] = {}
 	#T
+	def self.cars_per_color
+		@@cars_per_color
+	end
+
 	def self.total_car_count
 		@@total_car_count
 	end
@@ -10,9 +14,18 @@ class Car
 		"I'm a car! I've driven #{@distance} and have #{@fuel} gallons gas left"
 	end
 
-	def initialize()
+	def initialize
 		@fuel = 10
 		@distance = 0
+		@@total_car_count += 1
+		@best_color = best_color
+		@@cars_per_color << {x,y}
+	end
+
+	best_color = Car.most_popular_color
+
+	def most_popular_color
+			@@cars_per_color
 	end
 
 	def drive(miles)
@@ -44,3 +57,28 @@ car_a.drive(232)
 car_b.drive(117)
 puts car_a
 puts car_b
+
+#Step 6
+puts Car.total_car_count
+c1 = Car.new
+puts Car.total_car_count
+c2 = Car.new
+puts Car.total_car_count
+
+#Step 7
+	#1-Class method of instance
+	# c = Car.new
+	# c.total_car_count
+
+	#2-Class method on class
+	Car.total_car_count
+
+	#3-Instance method on class
+	# Car.drive(10)
+
+	#4-Instance method on instance
+	c = Car.new
+	c.drive(10)
+
+#Step 8
+Car.new(best_color)
